806c0baa502a45f0ab1dc4c1d505b8bb319c3946
vdi-terminal <vdi-terminal@EDU-WSB.local>
2025-05-31 11:45:42 +0200

    zmiana API w funkcji sumy

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-function sum(a, b) {
+/* function sum(a, b) {
     return a + b; // poprawka błędu -> PATCH
-  }
+  } -- TAK BYLO DLA PATCH
+*/
+
+function sum(...numbers) {
+  return numbers.reduce((acc, n) => acc + n, 0);
+}  // tak jest dla MAJOR
 
 function multiply(a, b) {
     return a * b;
-  }
+  } // A TO DLA MINOR
